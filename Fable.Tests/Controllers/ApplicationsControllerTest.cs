e9dc125d26edb5c2a5a16d03dc4872d5b159c1aa
jahabrewer <jahabrewer@gmail.com>
2015-03-01 22:17:14 -0800

    fixed user mocking

--- Fable.Tests/Controllers/ApplicationsControllerTest.cs
+++ Fable.Tests/Controllers/ApplicationsControllerTest.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Fable.Controllers;
@@ -51,14 +54,60 @@ namespace Fable.Tests.Controllers
             Mock<DbSet<Application>> mockSet = AsyncMocker.WrapAsAsyncCompatible(data, app => app.ApplicationId);
             var mockDbContext = new Mock<ApplicationDbContext>();
             mockDbContext.Setup(c => c.Applications).Returns(mockSet.Object);
-            var mockControllerContext = new Mock<ControllerContext>(MockBehavior.Strict);
-            mockControllerContext.Setup(cc => cc.HttpContext.User.Identity.Name).Returns(Guid.NewGuid().ToString());
             var controller = new ApplicationsController(mockDbContext.Object);
-            controller.ControllerContext = mockControllerContext.Object;
+            controller.ControllerContext = CreateMockControllerContextWithUser("bogus-user").Object;
 
             var result = await controller.Accept(10);
 
             Assert.IsType<HttpNotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Accept_ApplicationIsNotWaitingForDecision_ReturnsHttpForbidden()
+        {
+            var userId = Guid.NewGuid().ToString();
+            var data = new List<Application>
+            {
+                new Application
+                {
+                    ApplicationId = 10,
+                    ApplicationState = ApplicationState.Retracted,
+                    Absence = new Absence
+                    {
+                        AbsenceId = 1,
+                        Absentee = new ApplicationUser
+                        {
+                            Id = userId,
+                        },
+                        State = AbsenceState.Open,
+                    }
+                }
+            }.AsQueryable();
+            Mock<DbSet<Application>> mockSet = AsyncMocker.WrapAsAsyncCompatible(data, app => app.ApplicationId);
+            var mockDbContext = new Mock<ApplicationDbContext>();
+            mockDbContext.Setup(c => c.Applications).Returns(mockSet.Object);
+            var controller = new ApplicationsController(mockDbContext.Object);
+            controller.ControllerContext = CreateMockControllerContextWithUser(userId).Object;
+
+            var result = await controller.Accept(10);
+
+            Assert.IsType<HttpStatusCodeResult>(result);
+            Assert.Equal(((HttpStatusCodeResult) result).StatusCode, (int) HttpStatusCode.Forbidden);
+        }
+
+        private Mock<ControllerContext> CreateMockControllerContextWithUser(string userId)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId)
+            };
+            var genericIdentity = new GenericIdentity("");
+            genericIdentity.AddClaims(claims);
+            var genericPrincipal = new GenericPrincipal(genericIdentity, new string[0]);
+            var mockControllerContext = new Mock<ControllerContext>(MockBehavior.Strict);
+            mockControllerContext.Setup(cc => cc.HttpContext.User).Returns(genericPrincipal);
+
+            return mockControllerContext;
         }
     }
 }
